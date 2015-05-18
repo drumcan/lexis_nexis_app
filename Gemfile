3beedf1cf3f12cb7be1191619a6916f4dceff44e
Dan O'Connor <daniel.oconnor@me.com>
2015-05-18 16:57:52 -0500

    commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source "https://rubygems.org"
+gem 'sinatra', '1.0'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.7'
+end
+
+group :production do
+end
---
+++ app.rb
@@ -0,0 +1,70 @@
+require 'rubygems'
+require 'xml'
+require 'savon'
+require 'nokogiri'
+require 'sinatra'
+
+get "/lexis_nexis" do
+  erb :lexis_nexis
+end
+
+post "/upload" do
+
+client = Savon.client(basic_auth: ["PPBRXML", "Bp72jK3u"], wsdl: "https://wsonline.seisint.com/WsIdentity?ver_=1.85&wsdl")
+r = client.call( :flex_id, message: {
+     "Options" => {
+       "WatchLists" => "OFAC",
+       "RequireExactMatch" => {
+          "LastName" => "true",
+          "FirstName" => "true",
+          "Address" => "true"
+    }
+   },
+   "SearchBy" => {
+     "Name" => {
+      "First" => params[:first_name],
+      "Middle" => params[:middle_name],
+      "Last" => params[:last_name]
+      },
+    "Address" => {
+      "StreetNumber" => params[:st_num],
+      "StreePreDirection" => params[:direction],
+      "StreetName" => params[:st_name],
+      "StreetSuffix" => params[:st_suffix],
+      "UnitDesignation" => params[:unit_type],
+      "UnitNumber" => params[:unit_num],
+      "City" => params[:city],
+      "State" => params[:state],
+      "PostalCode" => params[:postal_code]
+    },
+    "DOB" => {
+      "Year" => params[:year],
+      "Month" => params[:month],
+      "Day" => params[:day]
+    },
+    "SSN" => params[:ssn],
+    "HomePhone" => params[:phone]
+   }
+  
+  })
+
+r = r.to_s
+xml = Nokogiri::XML(r)
+xml.remove_namespaces!
+transaction_id = xml.xpath('//TransactionId').text
+puts "Transaction id = #{transaction_id}"
+if ((xml.xpath('//VerifiedElementSummary/DOB').text ) == "1") 
+  puts "DOB MATCH => TRUE"
+else 
+  puts "DOB MATCH => FALSE"
+end
+if ((xml.xpath('//SSNDeceased').text) == "1") 
+  puts "SSN DECEASED = TRUE"
+else 
+  puts "SSN DECEASED = FALSE"
+end
+a = xml.xpath('//Description').each do |node|  puts "ERROR: #{node.text}" end 
+end
+
+
+
